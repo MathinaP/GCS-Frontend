cb8b1114eb51c6a4d597277eaeb69633fe1439f1
MathinaP <mathinasiraj2000@gmail.com>
2026-05-11 22:15:23 +0530

    seal added

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://gcs-backend-m19b.onrender.com',
         changeOrigin: true,
       },
     },
